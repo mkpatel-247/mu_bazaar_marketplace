2fe79975d44e7e1aa4365b4156229ac4cb649690
mkpatel-247 <meet.codes247@gmail.com>
2023-11-07 20:24:22 +0530

    Make some changes in database config

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env(SECRET_KEY)
+SECRET_KEY = "*5n^__3x*4bpwo1!eujaj4@dxme2dkbacue_ok8ls%wnk^9i)4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -80,7 +82,7 @@ WSGI_APPLICATION = 'marketplace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = env('DATABASE_URL')
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
@@ -133,5 +135,5 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 # ------------- Sender Email ID -------------------
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
